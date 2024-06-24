922def352702a6d89d05896504777f705c32e31f
quynhnhu1225 <quynhnhu1225@gmail.com>
2024-06-24 21:49:13 +0700

    update bikeshare.py

--- bikeshare.py
+++ bikeshare.py
@@ -8,7 +8,7 @@ import time
 import pandas as pd
 import numpy as np
 
-#Import file 
+## Import file to python
 
 CITY_DATA = { 'chicago': 'chicago.csv',
               'new york city': 'new_york_city.csv',
