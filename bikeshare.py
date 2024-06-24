7fe90e32124d7b36840214fc86de479a08eca868
quynhnhu1225 <quynhnhu1225@gmail.com>
2024-06-24 22:02:09 +0700

    delete unnecessary note

--- bikeshare.py
+++ bikeshare.py
@@ -23,7 +23,7 @@ def get_filters():
     """
     print('Hello! Let\'s explore some US bikeshare data!')
     
-    # TO DO: get user input for city (chicago, new york city, washington). HINT: Use a while loop to handle invalid inputs
+    # TO DO: get user input for city (chicago, new york city, washington). 
     
     city_valid = ['chicago','new york city','washington']
     
